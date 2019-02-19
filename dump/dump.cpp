da918c7dea661da6d83b602b41288a9d25ad8a26
Gerhard Gruber <gerhard.gruber@gdata.de>
2019-02-19 09:28:35 +0100

    Moved file processing to separate source file for easier testing.

---
+++ dump/dump.cpp
@@ -0,0 +1,219 @@
+// dump.cpp : Defines the entry point for the console application.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#include <iostream>
+
+#include "toolslib/files/FileFactory.h"
+#include "toolslib/files/File.h"
+
+#include "formatter/CA65Formatter.h"
+#include "formatter/EmptyFormatter.h"
+
+#include "dump.h"
+
+using namespace std;
+using namespace toolslib;
+using namespace toolslib::utils;
+using namespace toolslib::files;
+using namespace toolslib::strings;
+
+FileProcessor::FileProcessor(CommandlineParser &parser, int argc, char *argv[])
+: m_parser(parser)
+, m_result(0)
+, m_startPos(0)
+, m_maxLen(invalid64_t)
+, m_curLen(0)
+, m_formatter(make_unique<EmptyFormatter>())
+, m_input((new File())->setSTDIN(stdin))
+, m_output((new File())->setSTDOUT(stdout))
+{
+	createCommandlineOptions(m_parser);
+
+	if (!m_parser.parse(argc, argv))
+	{
+		uint32_t error = parser.getErrorIndex();
+		cerr << "Error with parameter '" << parser.getErrorParam() << "' at position " << to_string(error) << endl;
+		m_result = -1;
+	}
+}
+
+FileProcessor::~FileProcessor(void)
+{
+}
+
+toolslib::files::IFile *FileProcessor::createFile(const std::string &oFilename)
+{
+	Filename fn(oFilename);
+	return FileFactory::getInstance()->getFile(fn);
+}
+
+IFile *FileProcessor::openFile(const vector<string> &oArgs, const IFile::open_mode &oMode)
+{
+	const string &fn(oArgs[0]);
+	IFile *file = createFile(fn);
+
+	if (file == nullptr)
+	{
+		cerr << "Uknown filetype:" << fn << endl;
+		exit(0);
+	}
+
+	file->setOpenmode(oMode);
+	if (!file->open())
+	{
+		cerr << "Unable to open file:" << file->getOpenpath() << endl;
+		exit(0);
+	}
+
+	return file;
+}
+
+void FileProcessor::inputFile(CommandlineParser &oParser, const vector<string> &oArgs)
+{
+	UNUSED(oParser);
+	IFile::open_mode md = { 0 };
+
+	md.read = true;
+	md.binary = true;
+
+	m_input.reset(openFile(oArgs, md));
+}
+
+void FileProcessor::outputFile(CommandlineParser &oParser, const vector<string> &oArgs)
+{
+	UNUSED(oParser);
+
+	IFile::open_mode md = { 0 };
+
+	md.write = true;
+	md.binary = true;
+	md.truncate = true;
+	md.create = true;
+
+	m_output.reset(openFile(oArgs, md));
+}
+
+void FileProcessor::formatType(CommandlineParser &oParser, const vector<string> &oArgs)
+{
+	UNUSED(oParser);
+
+	cout << __func__ << endl;
+}
+
+void FileProcessor::skipOffset(CommandlineParser &oParser, const vector<string> &oArgs)
+{
+	UNUSED(oParser);
+
+	const string &number = oArgs[0];
+
+	int64_t ofs = fromNumber<int64_t>(number, nullptr);
+	if (ofs != invalid64_t)
+		m_input->seek(ofs, IFile::cur);
+}
+
+void FileProcessor::maxLength(CommandlineParser &oParser, const vector<string> &oArgs)
+{
+	UNUSED(oParser);
+
+	cout << __func__ << endl;
+}
+
+void FileProcessor::address(CommandlineParser &oParser, const vector<string> &oArgs)
+{
+	UNUSED(oParser);
+
+	cout << __func__ << endl;
+}
+
+void FileProcessor::createCommandlineOptions(CommandlineParser &oParser)
+{
+	oParser.setHeader(
+		"dump (C) by Gerhard Gruber\n"
+		"USAGE:\n"
+	);
+
+	oParser.addOption("help", "", "Print help")
+		.arguments()
+		;
+
+	oParser.addOption("input", "i", "Inputfile")
+		.mandatory()
+		.arguments()
+		.callback([&](CommandlineParser &oParser, const CommandlineParser::Option &oOption) { inputFile(oParser, oOption.values().back()); })
+		;
+
+	oParser.addOption("output", "o", "Outputfile")
+		.mandatory()
+		.arguments()
+		.callback([&](CommandlineParser &oParser, const CommandlineParser::Option &oOption) { outputFile(oParser, oOption.values().back()); })
+		;
+
+	oParser.addOption("type", "t", "Output format")
+		.arguments()
+		.callback([&](CommandlineParser &oParser, const CommandlineParser::Option &oOption) { formatType(oParser, oOption.values().back()); })
+		;
+
+	oParser.addOption("skip", "s", "Skip first N bytes from inputfile")
+		.arguments()
+		.callback([&](CommandlineParser &oParser, const CommandlineParser::Option &oOption) { skipOffset(oParser, oOption.values().back()); })
+		;
+
+	oParser.addOption("length", "l", "Write only N bytes")
+		.arguments()
+		.callback([&](CommandlineParser &oParser, const CommandlineParser::Option &oOption) { maxLength(oParser, oOption.values().back()); })
+		;
+
+	oParser.addOption("address", "a", "Write first two bytes with address.")
+		.arguments()
+		.callback([&](CommandlineParser &oParser, const CommandlineParser::Option &oOption) { address(oParser, oOption.values().back()); })
+		;
+}
+
+bool FileProcessor::hasHelp(void)
+{
+	if (m_parser.hasArgument("help"))
+	{
+		m_parser.help();
+		return true;
+	}
+
+	return false;
+}
+
+int FileProcessor::status(void)
+{
+	return m_result;
+}
+
+int FileProcessor::run(void)
+{
+	if (m_result)
+		return m_result;
+
+	char buffer[1024];
+
+	while (!m_input->isEOF())
+	{
+		int64_t rd = m_input->read(buffer, 1024);
+		if (rd <= 0)
+		{
+			if (rd < 0)
+				cerr << "Error reading " << m_input->getOpenpath() << endl;
+
+			break;
+		}
+
+		rd = m_output->write(buffer, rd);
+		if (rd < 0)
+		{
+			cerr << "Error writing " << m_input->getOpenpath() << endl;
+			break;
+		}
+	}
+
+	return 0;
+}
---
+++ dump/include/dump.h
@@ -0,0 +1,86 @@
+#ifndef DUMP_H
+#define DUMP_H
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "toolslib/utils/CommandlineParser.h"
+#include "toolslib/files/IFile.h"
+#include "toolslib/strings/numbers.h"
+
+class Formatter;
+
+template <typename T>
+T fromNumber(const char *number, const char *end, const char **scanned)
+{
+	// TODO: Trim std::string
+
+	if (number >= end)
+		throw invalid_argument("Number is empty");
+
+	size_t len = end - number;
+	if (len == 1)
+		return toolslib::strings::fromDecimal<T>(number, end, scanned);
+
+	char c = number[2];
+	if (len == 2 && (c == 'x' || c == 'X'))
+		throw invalid_argument("Empty hex std::string");
+
+	if (*(end - 1) == 'h')
+		return toolslib::strings::fromHex<T>(number, end-1, scanned);
+
+	if(number[0] == '0' && (c == 'x' || c == 'X'))
+		return toolslib::strings::fromHex<T>(&number[2], end, scanned);
+
+	if (number[0] == '$')
+		return toolslib::strings::fromHex<T>(&number[1], end, scanned);
+
+	if(number[0] == '%')
+		return toolslib::strings::fromBinary<T>(&number[1], end, scanned);
+
+	return toolslib::strings::fromDecimal<T>(number, end, scanned);
+}
+
+template <typename T>
+T fromNumber(const std::string& number, const char **scanned)
+{
+	return fromNumber<T>(&number[0], &number[number.size()], scanned);
+}
+
+class FileProcessor
+{
+public:
+	FileProcessor(toolslib::utils::CommandlineParser &parser, int argc, char *argv[]);
+	virtual ~FileProcessor();
+
+	bool hasHelp(void);
+	int run(void);
+	int status(void);
+
+protected:
+	virtual toolslib::files::IFile *createFile(const std::string &oFilename);
+
+	toolslib::files::IFile *openFile(const std::vector<std::string> &oArgs, const toolslib::files::IFile::open_mode &oMode);
+	void inputFile(toolslib::utils::CommandlineParser &oParser, const std::vector<std::string> &oArgs);
+	void outputFile(toolslib::utils::CommandlineParser &oParser, const std::vector<std::string> &oArgs);
+	void formatType(toolslib::utils::CommandlineParser &oParser, const std::vector<std::string> &oArgs);
+	void skipOffset(toolslib::utils::CommandlineParser &oParser, const std::vector<std::string> &oArgs);
+	void maxLength(toolslib::utils::CommandlineParser &oParser, const std::vector<std::string> &oArgs);
+	void address(toolslib::utils::CommandlineParser &oParser, const std::vector<std::string> &oArgs);
+	void createCommandlineOptions(toolslib::utils::CommandlineParser &oParser);
+
+private:
+	std::unique_ptr<toolslib::files::IFile> m_input;
+	std::unique_ptr<toolslib::files::IFile> m_output;
+	std::unique_ptr<Formatter> m_formatter;
+	int m_result;
+	int64_t m_startPos;
+	int64_t m_maxLen;
+	int64_t m_curLen;
+	toolslib::utils::CommandlineParser &m_parser;
+};
+
+#endif // DUMP_H
